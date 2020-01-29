cf933439c43daee2e222fe50e71a4d84f0196ce4
cjanarthanangit <janaece_12@rocketmail.com>
2020-01-29 10:24:31 +0530

    Ini

---
+++ dfbdf.php
@@ -0,0 +1,9 @@
+<?php
+print_r(cal_info(0));
+echo" Next one".'<br>'.'<br>';
+print_r(cal_info(1));
+echo"\n Next two".'<br>'.'<br>';
+print_r(cal_info(2));
+echo"\n Next three".'<br>'.'<br>';
+print_r(cal_info(3));
+?>
